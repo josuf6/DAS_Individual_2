36f0b0e51aa053c6d418a33c53e031e23b82fa59
josuf6 <josuf6@gmail.com>
2022-03-26 13:15:40 +0100

    Menu continentes

---
+++ app/src/main/java/com/example/das_individual_1/AdaptadorListView.java
@@ -0,0 +1,52 @@
+package com.example.das_individual_1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class AdaptadorListView extends BaseAdapter {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private String[] continentes;
+    private int[] imagenes;
+
+    public AdaptadorListView(Context pContext, String[] pContinentes, int[] pImagenes) {
+        this.context = pContext;
+        this.continentes = pContinentes;
+        this.imagenes = pImagenes;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return continentes.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return continentes[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = this.inflater.inflate(R.layout.fila,null);
+        TextView nombre = (TextView) view.findViewById(R.id.nombre_continente);
+        ImageView img =(ImageView) view.findViewById(R.id.imagen_continente);
+
+        nombre.setText(continentes[i]);
+        img.setImageResource(imagenes[i]);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/das_individual_1/MainActivity.java
+++ app/src/main/java/com/example/das_individual_1/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.das_individual_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView continentes;
+    String[] nombres = {"África", "América", "Asia", "Europa", "Oceanía"};
+    int[] imagenes = {R.drawable.africa, R.drawable.america, R.drawable.asia, R.drawable.europa, R.drawable.oceania};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continentes = (ListView) findViewById(R.id.continentes); //crea el ListView en el que se mostrarán continentes con iconos y nombres
+        AdaptadorListView adaptadorLV = new AdaptadorListView(getApplicationContext(), nombres, imagenes); //Al ser un ListView personalizado se utiliza un objeto adaptador
+        continentes.setAdapter(adaptadorLV); //se relaciona el adaptador con el ListView
     }
 }
